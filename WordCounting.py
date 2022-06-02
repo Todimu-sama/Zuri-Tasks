3796e4bb3d2f78f35c554668ef1c3c5c9525727c
Oluwatodimu <todimuisewon@gmail.com>
2022-06-02 12:44:30 +0100

    none

--- WordCounting.py
+++ WordCounting.py
@@ -3,6 +3,9 @@
 
 # read file content and return string of the file contents
 
+import string
+
+
 def read_file_content(filename: str) -> str:
     with open(filename, "r") as file:
         fileText = file.read()
@@ -15,6 +18,20 @@ def count_words() -> dict:
     text: str = read_file_content("./story.txt")
     wordFrequencies: dict = dict()
     wordsInText = text.split(" ")
+
+    for line in text:
+        # Remove the leading spaces and newline character
+        line = line.strip()
+
+        # Convert the characters in line to
+        # lowercase to avoid case mismatch
+        line = line.lower()
+
+        # Remove the punctuation marks from the line
+        line = line.translate(line.maketrans("", "", string.punctuation))
+
+        # Split the line into words
+        words = line.split(" ")
 
     for word in wordsInText:
         if word in wordFrequencies:
